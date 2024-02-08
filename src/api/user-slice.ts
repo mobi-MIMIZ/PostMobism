f1659637b4e6112cbd4b6edd4854d5c92356299f
55555-Jyeon <jyeon380516@gmail.com>
2024-02-08 22:03:31 +0900

    edit : rtk user-slice

--- src/api/user-slice.ts
+++ src/api/user-slice.ts
@@ -3,20 +3,8 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import { axiosInstance } from "./core.api"
 import { SignUpUser } from "@/type/dto/form"
 
-const PATH = "/user"
-
-type UserState = {
-  data: User[] | null
-  loading: boolean
-  error: string | null
-}
-
-const initialState: UserState = {
-  data: null,
-  loading: false,
-  error: "",
-}
-
+// test
+// 원하는 페이지에 useEffect 사용해 console.log에 찍으면 잘 찍힙니다 :)
 export const getUsers = createAsyncThunk<User[]>("user", async () => {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/users")
@@ -26,6 +14,8 @@ export const getUsers = createAsyncThunk<User[]>("user", async () => {
     throw new Error("사용자 데이터를 불러오는 데 실패했습니다!")
   }
 })
+
+const PATH = "/user"
 
 export const SignUp = createAsyncThunk("user", async ({ ...formData }: SignUpUser) => {
   const { userId, password, nickname } = formData
@@ -43,29 +33,22 @@ export const SignUp = createAsyncThunk("user", async ({ ...formData }: SignUpUse
   }
 })
 
+const initialStateValue = { userId: "", nickName: "" }
+
 export const userSlice = createSlice({
   name: "user",
-  initialState,
+  initialState: { value: initialStateValue },
   reducers: {
-    // signIn: (state, action: PayloadAction<SignInUser[]>) => {state.data = action.payload},
-    // signUp: (state, action: PayloadAction<SignUpUser[]>) => {state.data = action.payload},
-  },
-  extraReducers(builder) {
-    builder
-      .addCase(getUsers.pending, state => {
-        state.loading = true
-      })
-      .addCase(getUsers.fulfilled, (state, action) => {
-        state.loading = false
-        state.error = null
-        state.data = action.payload
-      })
-      .addCase(getUsers.rejected, (state, action) => {
-        state.loading = false
-        state.error = action.error.message || "예기치 못한 에러로 사용자 데이터를 불러오지 못했습니다!"
-        state.data = null
-      })
+    // signIn: (state, action: PayloadAction<SignInUser[]>) => {
+    //   state.data = action.payload
+    // },
+    signUp: (state, action) => {
+      state.value = action.payload
+    },
+    signOut: state => {
+      state.value = initialStateValue
+    },
   },
 })
-
+export const { signUp, signOut } = userSlice.actions
 export default userSlice.reducer
